744f46bc82349ac508b794b336d84836227f1314
Your Name <you@example.com>
2025-06-15 15:13:06 +0300

    dummy

--- test.java
+++ test.java
@@ -11,31 +11,23 @@ public class test {
             fibNodes[i] = fib.insert(i + 1, Integer.toString(i + 1));
         }
         java.util.Random rand = new java.util.Random(2025);
-        int remaining = numberOfInserts;
-        while (remaining > 0) {
-            // Decrease keys
-            for (int i = 0; i < DECREASE_KEYS_PER_LOOP && remaining > 0; i++) {
-                int idx = rand.nextInt(numberOfInserts);
-                
-                if (fibNodes[idx] != null && fibNodes[idx].key > Integer.MIN_VALUE + 1) {
-                    int diff = fibNodes[idx].key + 1 + rand.nextInt(1000); // make negative
-                    fib.decreaseKey(fibNodes[idx], diff);
-                }
-            }
-            System.out.println("size: " + fib.size());
-
-            // printHeap.printFibonacciHeap(fib);
-
-            fib.deleteMin();
 
 
-            // for (int i = 0; i < DELETE_MINS_PER_LOOP && remaining > 0; i++) {
-            //     fib.deleteMin();
-            //     remaining--;
-            // }
-            // // Print heap after each loop
-            System.out.println("Heap after loop:");
-            printHeap.printFibonacciHeap(fib);
+        System.out.println("After inserts:");
+        printHeap.printFibonacciHeap(fib);
+
+        System.out.println("Now going to decrease keys");
+        for (int i = 0; i < DECREASE_KEYS_PER_LOOP; i++) {
+            int idx = rand.nextInt(numberOfInserts);
+
+            int diff = 100;
+            fib.decreaseKey(fibNodes[idx], diff);
         }
+        System.out.println("Heap after loop:");
+        printHeap.printFibonacciHeap(fib);
+
+        System.out.println("Now deleting min");
+        fib.deleteMin();
+        printHeap.printFibonacciHeap(fib);
     }
 }
